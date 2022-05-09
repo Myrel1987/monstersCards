c0ff8cc5f23651bf0d6fc6a445e21cdb08b4d58b
Apetrei <myrel1987@gmail.com>
2022-05-09 12:41:15 +0200

    updated to typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 
-import { CardList } from "./components/card-list/card-list.component";
+import CardList from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
+
+import { getData } from "./components/utils/data.utils";
 import "./App.css";
+
+// we define the type that we are fetching from the url
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setMonsters(users);
+    };
+
+    fetchUsers();
   }, []);
 
   useEffect(() => {
@@ -22,7 +36,7 @@ const App = () => {
     setFilterMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searcFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searcFieldString);
   };
